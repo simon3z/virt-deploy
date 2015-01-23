8bf9012f59aa0fe7142c3b13a924156b27b6b3b2
Federico Simoncelli <fsimonce@redhat.com>
2015-01-24 00:28:58 +0100

    utils: add support for waiting instance access
    Fixes #5
    
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>

--- virtdeploy/test_utils.py
+++ virtdeploy/test_utils.py
@@ -22,7 +22,11 @@ from __future__ import absolute_import
 
 import unittest
 
+from mock import MagicMock
+from mock import call
 from mock import patch
+from socket import SOL_SOCKET
+from socket import SO_ERROR
 from subprocess import CalledProcessError
 
 from . import utils
@@ -61,3 +65,141 @@ class TestExecute(unittest.TestCase):
                 utils.execute(command, **optargs)
 
             self.assertEqual(cm.exception.returncode, 1)
+
+
+class TestMonotonicTime(unittest.TestCase):
+    def test_monotonic_time(self):
+        self.assertEqual(type(utils.monotonic_time()), float)
+
+
+class TestWaitTcpAccess(unittest.TestCase):
+    TIMEOUT = 180.0
+    MININT = 5.0
+    HALFMININT = MININT / 2.0
+    MAXINT = 10.0
+
+    EXERCISES = (
+        {
+            'monotime': [0, 0, 0, TIMEOUT],
+            'probearg': [MAXINT],
+            'proberet': [None],
+            'retvalue': None,
+            'sleepexp': [call(MININT)],
+        },
+        {
+            'monotime': [0, 0, (MININT - HALFMININT), TIMEOUT],
+            'probearg': [MAXINT],
+            'proberet': [None],
+            'retvalue': None,
+            'sleepexp': [call(HALFMININT)],
+        },
+        {
+            'monotime': [0, 0, MININT, TIMEOUT],
+            'probearg': [MAXINT],
+            'proberet': [None],
+            'retvalue': None,
+            'sleepexp': [call(0)],
+        },
+        {
+            'monotime': [0, 0, (MININT + 1.0), TIMEOUT],
+            'probearg': [MAXINT],
+            'proberet': [None],
+            'retvalue': None,
+            'sleepexp': [call(0)],
+        },
+        {
+            'monotime': [0, 0, (MININT - HALFMININT), (TIMEOUT - MININT),
+                         (TIMEOUT - MININT + HALFMININT), TIMEOUT],
+            'probearg': [MAXINT, MININT],
+            'proberet': [None, None],
+            'retvalue': None,
+            'sleepexp': [call(HALFMININT)],
+        },
+        {
+            'monotime': [0, 0, MININT, MAXINT],
+            'probearg': [MAXINT],
+            'proberet': [None, '192.168.122.2'],
+            'retvalue': '192.168.122.2',
+            'sleepexp': [call(0)],
+        },
+    )
+
+    @patch('time.sleep')
+    @patch('virtdeploy.utils.monotonic_time')
+    @patch('virtdeploy.utils.probe_tcp_access')
+    def test_wait_tcp_access(self, probe_mock, monotime_mock, sleep_mock):
+        for exercise in self.EXERCISES:
+            probe_mock.side_effect = exercise['proberet']
+            monotime_mock.side_effect = exercise['monotime']
+
+            retvalue = utils.wait_tcp_access(None, None,
+                                             timeout=self.TIMEOUT,
+                                             mininterval=self.MININT,
+                                             maxinterval=self.MAXINT)
+
+            probe_mock.called_with(timeout=exercise['probearg'])
+            self.assertEqual(retvalue, exercise['retvalue'])
+            self.assertEqual(sleep_mock.mock_calls, exercise['sleepexp'])
+
+            sleep_mock.reset_mock()
+
+
+class TestProbeTcpAccess(unittest.TestCase):
+    TIMEOUT = 10
+
+    def setUp(self):
+        self.addresses = ['192.168.122.2', '192.168.122.3']
+        self.sockets = [MagicMock() for _ in self.addresses]
+        self.driver_mock = MagicMock()
+        self.driver_mock.instance_address.return_value = self.addresses
+
+    @patch('select.select')
+    @patch('socket.socket')
+    @patch('virtdeploy.utils.monotonic_time')
+    def test_probe_timeout(self, time_mock, socket_mock, select_mock):
+        socket_mock.side_effect = self.sockets
+        time_mock.side_effect = [0, 0, self.TIMEOUT / 2, self.TIMEOUT]
+        select_mock.return_value = (), (), ()
+
+        retvalue = utils.probe_tcp_access(self.driver_mock, None,
+                                          timeout=self.TIMEOUT)
+
+        self.assertEqual(retvalue, None)
+
+        self._assert_sockets_calls()
+
+        # FIXME: mock should record the collection snapshot, not the
+        # collection object, code should be:
+        #   call((), self.sockets, (), self.TIMEOUT)
+        select_mock.assert_has_calls([
+            call((), [], (), self.TIMEOUT),
+            call((), [], (), self.TIMEOUT / 2),
+        ])
+
+    @patch('select.select')
+    @patch('socket.socket')
+    @patch('virtdeploy.utils.monotonic_time')
+    def test_probe_success(self, time_mock, socket_mock, select_mock):
+        sock = self.sockets[0]
+
+        sock.getsockopt.return_value = 0
+        sock.getpeername.return_value = self.addresses[0]
+
+        socket_mock.side_effect = self.sockets
+        time_mock.side_effect = [0, 0]
+        select_mock.return_value = (), (sock,), ()
+
+        retvalue = utils.probe_tcp_access(self.driver_mock, None,
+                                          timeout=self.TIMEOUT)
+
+        self.assertEqual(retvalue, self.addresses[0])
+
+        self._assert_sockets_calls()
+
+        sock.getsockopt.assert_called_with(SOL_SOCKET, SO_ERROR)
+
+    def _assert_sockets_calls(self):
+        for sock, address in zip(self.sockets, self.addresses):
+            sock.setblocking.assert_called_once_with(0)
+            sock.connect_ex.assert_called_once_with((address, 22))
+            sock.close.assert_called_once_with()
--- virtdeploy/utils.py
+++ virtdeploy/utils.py
@@ -20,9 +20,13 @@
 
 from __future__ import absolute_import
 
+import os
 import random
+import select
+import socket
 import string
 import subprocess
+import time
 
 _PASSWORD_CHARS = string.ascii_letters + string.digits + '!#$%&'
 
@@ -41,3 +45,67 @@ def execute(args, stdout=None, stderr=None, cwd=None):
 def random_password(size=12):
     chars = (random.choice(_PASSWORD_CHARS) for _ in range(size))
     return ''.join(chars)
+
+
+def monotonic_time():
+    return os.times()[4]
+
+
+def probe_tcp_access(driver, vmid, port=22, timeout=10):
+    sockets = list()
+    endtime = monotonic_time() + timeout
+
+    for address in driver.instance_address(vmid):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(0)
+        sock.connect_ex((address, port))
+        sockets.append(sock)
+
+    address_found = None
+
+    while sockets and address_found is None:
+        remaining = endtime - monotonic_time()
+
+        if remaining <= 0:
+            break
+
+        _, response, _ = select.select((), sockets, (), remaining)
+
+        for sock in response:
+            e = sock.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR)
+
+            if e == 0:
+                address_found = sock.getpeername()
+
+            sock.close()
+            sockets.remove(sock)
+
+    for sock in list(sockets):
+        sock.close()
+        sockets.remove(sock)
+
+    return address_found
+
+
+def wait_tcp_access(driver, vmid, port=22, timeout=180,
+                    mininterval=5.0, maxinterval=10.0):
+    endtime = monotonic_time() + timeout
+
+    while True:
+        probetime = monotonic_time()
+        remaining = min(maxinterval, endtime - probetime)
+
+        if remaining <= 0:
+            return None
+
+        address_found = probe_tcp_access(driver, vmid, port, remaining)
+
+        if address_found is not None:
+            return address_found
+
+        nexttry = probetime + mininterval
+
+        if nexttry >= endtime:
+            return None
+
+        time.sleep(max(0, nexttry - monotonic_time()))
